852e592199d6d7b5e9a38d8d53a47ad71d8669c4
Mike Johnson <mike@mapleprintservices.com>
2022-11-30 23:11:47 -0500

    put and delete methods in place work in Insomnia not writing to db yet

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const notes = require('../db/db.json');
 const uuid = require('uuid');
+const fs = require('fs');
 
 // get notes
 router.get('/', (req, res) => {
@@ -23,7 +24,33 @@ router.post('/', (req, res) => {
   res.json(notes);
 });
 
-// delete a note
+// change a note
+router.put('/:id', (req, res) => {
+  const found = notes.some(note => note.id === req.params.id);
 
+  if (found) {
+    const updNote = req.body;
+    notes.forEach(note => {
+      if (note.id === req.params.id){
+        note.title = updNote.title ? updNote.title : note.title;
+        note.text = updNote.text ? updNote.text : note.text;
+        res.json({msg: 'Note updated', note})
+      }
+    });
+  } else {
+    res.status(400).json({msg: 'This note does not exist'});
+  }
+});
+
+// delete a note
+router.delete('/:id', (req, res) => {
+  const found = notes.some(note => note.id === req.params.id);
+
+  if (found) {
+    res.json({ msg: 'Note deleted', notes: notes.filter(note => note.id !== req.params.id)});
+  } else {
+    res.status(400).json({msg: 'This note does not exist'});
+  }
+});
 
 module.exports = router;
